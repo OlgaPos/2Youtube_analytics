b3bccd4c80319498043490e5c4903718c078a21e
Olga_Pos <olga-posp@yandex.ru>
2023-03-14 22:29:43 +0100

    commit hw14-01

--- utils/youtube.py
+++ utils/youtube.py
@@ -17,7 +17,7 @@ class Youtube:
 
 class Channel:
 
-    def __init__(self, __channel_id: str):
+    def __init__(self, channel_id: str):
         self.__channel_id = channel_id
 
         channel = Youtube.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
@@ -45,6 +45,31 @@ class Channel:
         with open(path, 'w', encoding="UTF-8") as file:
             json.dump(self.info, file, indent=2)
 
+    def __str__(self):
+        """Выводим название Youtube-канала пользователю"""
+        print(f"Youtube-канал: {self.title}")
+
+    def __gt__(self, other):
+        """Сравниваем больше ли у первого Youtube-канала подписчиков, чем у другого"""
+        if isinstance(other, Channel):
+            return self.subscriber_count > other.subscriber_count
+        else:
+            raise ValueError("Неправильный формат")
+
+    def __lt__(self, other):
+        """Сравниваем меньше ли у первого Youtube-канала подписчиков, чем у другого"""
+        if isinstance(other, Channel):
+            return self.subscriber_count < other.subscriber_count
+        else:
+            raise ValueError("Неправильный формат")
+
+    def __add__(self, other):
+        """Складываем подписчиков первого Youtube-канала и другого"""
+        if isinstance(other, Channel):
+            return self.subscriber_count + other.subscriber_count
+        else:
+            raise ValueError("Неправильный формат")
+
     # def print_info(self):
     #     channel = Youtube.youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
     #     print(json.dumps(channel, indent=2, ensure_ascii=False))
@@ -56,32 +81,44 @@ class Channel:
 
 
 # channel_id = 'UC86DMwzxzk1DWqnaAjibUHQ'  # Уральские пельмени
-channel_id = 'UCsT0YIqwnpJCM-mx7-gSA4Q'  # TEDx Talks
-
-# 2 экземпляра класса
-# ural_pelmeni = Channel('UC86DMwzxzk1DWqnaAjibUHQ')
-# ural_pelmeni.print_info()
-
-tedx_talks = Channel('UCsT0YIqwnpJCM-mx7-gSA4Q')
-# tedx_talks.print_info()
-# print(tedx_talks.create_data())
-
-# print(type(tedx_talks.create_data()))  # Узнаём тип data
-# data_dict = json.loads(tedx_talks.create_data())  # Переводим data в формат словаря для создания json
-# print(type(data_dict))
+# channel_id = 'UCsT0YIqwnpJCM-mx7-gSA4Q'  # TEDx Talks
 #
+# # 2 экземпляра класса
+# # ural_pelmeni = Channel('UC86DMwzxzk1DWqnaAjibUHQ')
+# # ural_pelmeni.print_info()
+#
+# tedx_talks = Channel('UCsT0YIqwnpJCM-mx7-gSA4Q')
+# # tedx_talks.print_info()
+# # print(tedx_talks.create_data())
+#
+# # print(type(tedx_talks.create_data()))  # Узнаём тип data
+# # data_dict = json.loads(tedx_talks.create_data())  # Переводим data в формат словаря для создания json
+# # print(type(data_dict))
+# #
+#
+#
+# # получаем значения атрибутов
+# print(tedx_talks.title)
+# print(tedx_talks.video_count)
+# print(tedx_talks.url)
+#
+# # менять не можем
+# tedx_talks.channel_id = 'Новое название'
+#
+# # можем получить объект для работы с API вне класса
+# print(Channel.get_service())
+#
+# # создать файл 'tedx_talks.json' в данными по каналу
+# tedx_talks.to_json('tedx_talks.json')
 
+ch1 = Channel('UCsT0YIqwnpJCM-mx7-gSA4Q')
+ch2 = Channel('UC86DMwzxzk1DWqnaAjibUHQ')
 
-# получаем значения атрибутов
-print(tedx_talks.title)
-print(tedx_talks.video_count)
-print(tedx_talks.url)
+print(ch1)
+print(ch2)
 
-# менять не можем
-tedx_talks.channel_id = 'Новое название'
+ch1 > ch2
 
-# можем получить объект для работы с API вне класса
-print(Channel.get_service())
+ch1 < ch2
 
-# создать файл 'tedx_talks.json' в данными по каналу
-tedx_talks.to_json('tedx_talks.json')
+ch1 + ch2
